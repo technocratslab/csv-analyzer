3bffd75e08145243f1ed4735fda436a616b63841
Mohib Wasay <wasay_siddiqui81@yahoo.com>
2023-04-16 22:06:19 +0200

    fix: button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -14,7 +14,7 @@ export const Button: FC<ButonProps> = ({
     onClick={onClick}
     type="button"
     disabled={disabled}
-    className={`text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none dark:focus:ring-blue-800 mr-0 ${
+    className={`text-white font-medium rounded-lg text-sm px-5 py-2.5 mb-2 focus:outline-none dark:focus:ring-blue-800 mr-0 ${
       (disabled || loading)
         ? "cursor-not-allowed bg-blue-400"
         : "bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700"
