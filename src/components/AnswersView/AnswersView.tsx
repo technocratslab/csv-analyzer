98835bd857c57c0c409f4ba9061aa7444be09245
faizan <muhammad.faizan@timify.com>
2023-04-17 06:49:09 +0200

    added icons, added chat history context.

--- src/components/AnswersView/AnswersView.tsx
+++ src/components/AnswersView/AnswersView.tsx
@@ -1,6 +1,6 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FcManager } from "react-icons/fc";
 import { FC } from "react";
-
+import Image from 'next/image'
 export type AnswerResults = {
   question: string;
   answer: React.ReactNode;
@@ -11,19 +11,29 @@ export type AnswerViewProps = {
 };
 
 export const AnswersView: FC<AnswerViewProps> = ({ results }) => {
+  if (results.length === 0) {
+    return <></>
+  }
   return (
-    <div>
-      {results && <h1 className="text-xl font-bold my-4">List of Answers</h1>}
-
+    <div className="p-2 border-2 border-blue-700 rounded-lg">
+      <h1 className="text-xl font-bold my-4">List of Answers</h1>
       {results.map((result, index) => (
         <div
-          className="border-2 border-blue-700 rounded-lg p-2 mb-4"
+          className="mb-4"
           key={String(index)}
         >
-          <p className="mb-2 text-md">{result.question}</p>
+          <p className="mb-2 text-md"><FcManager className="inline mr-2 text-xl"></FcManager>{result.question}</p>
           <p>
-            <strong className="text-md font-black">Answer: </strong> <br />
-            {result.answer}
+            <strong className="text-md font-black">Answer: </strong><br />
+            <p className="">
+              <Image 
+                className="inline mr-2 h-6 w-auto"
+                src="/chat-bot.png"
+                alt="chat-bot"
+                width={300}
+                height={300}
+              />
+              <span className="text-base">{result.answer}</span></p>
           </p>
         </div>
       ))}
